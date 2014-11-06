7208c573282ad68868efa622a87e8b34a188b892
You Kobayashi <sweep.3092@gmail.com>
2014-11-06 13:40:02 +0900

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd ~
+ln -s dotfiles/_zshrc .zshrc
+ln -s dotfiles/_gitconfig .gitconfig
+ln -s dotfiles/_gitignore .gitignore
+ln -s dotfiles/_tmux.conf .tmux.conf
+ln -s dotfiles/_vimrc .vimrc
