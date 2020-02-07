138aecded218d09ffa7b1ab81628b48825695346
Yu Kobayashi <sweep.3092@gmail.com>
2020-02-07 10:41:02 +0900

    update ghq path

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,4 +10,4 @@ ln -s dotfiles/_vimrc .vimrc
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 go get github.com/peco/peco/cmd/peco/
-go get github.com/motemen/ghq
+go get github.com/x-motemen/ghq
