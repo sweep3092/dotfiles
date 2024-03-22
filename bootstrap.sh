ca1907208cf48da7dfe870801dffb50d413de134
Yu Kobayashi <sweep.3092@gmail.com>
2024-03-23 08:20:20 +0900

    enable starship

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,8 @@ ln -s dotfiles/_gitconfig .gitconfig
 ln -s dotfiles/_gitignore .gitignore
 ln -s dotfiles/_tmux.conf .tmux.conf
 ln -s dotfiles/_vimrc .vimrc
+mkdir ~/.config
+ln -s dotfiles/starship.toml ~/.config/starship.toml
 
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
@@ -13,9 +15,16 @@ git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 case ${OSTYPE} in
     darwin*)
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        brew install git vim tig zsh ruby rbenv ruby-build tmux ghq peco postgresql redis go awscli zsh-completions
+        brew tap homebrew/cask-fonts
+
+        brew install git vim tig zsh ghq peco zsh-completions
+        brew install --cask docker iterm2 vlc zoom visual-studio-code notion google-chrome slack figma 1password google-japanese-ime
+        brew install font-hack-nerd-font
+
+        chsh -s /usr/local/bin/zsh
         ;;
     linux*)
         alias ls='ls --color'
         ;;
 esac
+
