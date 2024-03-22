834f0cec8ced0010c34c72c252a5eadde1be7610
Yu Kobayashi <sweep.3092@gmail.com>
2024-03-23 08:27:32 +0900

    add iterm2_preference

--- install_homebrew.sh
+++
@@ -1,6 +0,0 @@
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-
-brew install zsh-completions git tig lua node rbenv ruby-build go
-brew install vim --with-lua
-
